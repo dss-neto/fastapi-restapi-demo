e808353cd16a5dfd0e5bf11184bb66f51eebc0b3
dss-neto <192475519+dss-neto@users.noreply.github.com>
2026-04-06 21:21:05 -0300

    Update formatting to match how data is now processed

---
+++ src/backend/tertiary/formatter.py
@@ -0,0 +1,13 @@
+def format_user_data(user_data: list):
+    formatted_user_data = {
+        "id": user_data.id,
+        "name": user_data.name,
+        "email": user_data.email,
+    }
+
+    return formatted_user_data
+
+
+def format_user_list(user_list: list):
+    formatted_user_list = [format_user_data(user) for user in user_list]
+    return formatted_user_list
