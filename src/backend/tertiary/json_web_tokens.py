f34c9f2970914c21d1692cc68d10175cacaceb19
dss-neto <192475519+dss-neto@users.noreply.github.com>
2026-04-06 21:23:17 -0300

    Import secret key from a .env file

---
+++ src/backend/tertiary/json_web_tokens.py
@@ -0,0 +1,36 @@
+import jwt
+from tertiary.validation import (
+    validate_token,
+    raise_unauthorized_error,
+)
+from dotenv import load_dotenv
+from os import getenv
+
+load_dotenv()
+SECRET_KEY = getenv("secret_key")
+
+
+def generate_user_token(user_id: int, user_email: str):
+    payload_data = {"sub": str(user_id), "email": user_email}
+    token = jwt.encode(
+        payload=payload_data,
+        key=SECRET_KEY,
+    )
+
+    return token
+
+
+def decode_jwt_token(token):
+    decoded = jwt.decode(token, SECRET_KEY, ["HS256"])
+    return decoded
+
+
+def get_decoded_token(request: object):
+    auth_header = request.headers.get("Authorization")  # "Bearer {token}"
+    validate_token(auth_header)
+    jwt_token = auth_header.split()[1]  # ["Bearer", "{token}"]
+    try:
+        decoded_token_data = decode_jwt_token(jwt_token)
+    except jwt.exceptions.DecodeError:
+        raise_unauthorized_error()
+    return decoded_token_data
