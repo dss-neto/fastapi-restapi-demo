bd409eae5e504a1fecc4b5c5951b22cc37b1662d
dss-neto <192475519+dss-neto@users.noreply.github.com>
2026-04-30 08:26:52 -0300

    Add FastAPI dependency

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,35 +1,25 @@
-# TODO: ADD TESTING WITH PYTEST
+
 # TODO: ROLES:
 #             ADMINS CAN DELETE AND READ USER DB
 #             GENERAL USERS CAN ONLY CREATE AND DELETE THEIR OWN ROW IN USER DB
 # TODO: SIMPLE FRONT END
 
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Depends
+from sqlalchemy.orm import Session
+from src.backend.database.db import engine
 
 # APIs is the mean of communication between client and server/backend
 # it acts like a restaurant waiter, where the client is the customer and the server is the kitchen
 from fastapi.params import Body
 from pydantic import BaseModel
-
 # BaseModel makes it so fastapi automatically validates, parses and request from body
-from operations.user_operations import (
-    operation_register_user,
-    operation_read_user_list,
-    operation_login_user,
-    operation_delete_user,
-)
-from operations.task_operations import (
-    operation_create_task,
-    operation_get_task_list,
-    operation_get_single_task,
-    operation_update_task,
-    operation_delete_task,
-)
-from tertiary.json_web_tokens import (
-    get_decoded_token,
-)
+
 
 app = FastAPI()
+
+def get_session():
+    with Session(engine) as session:
+        yield session
 
 # = = = = USERS = = = =
 
@@ -41,30 +31,56 @@ class UserSchema(BaseModel):
     # basemodel also makes pydantic parse data
     # example: {"name": "Bob"} -> "Bob"
     # pasing: convert raw data to usable data
+    
+class UserLoginSchema(BaseModel):
+    email: str
+    password: str
+
 
 
 @app.post("/register")
 # /register is the route
-def register_user(user: UserSchema = Body(...)):
+def register_user(
+    user: UserSchema = Body(...),
+    session: Session = Depends(get_session)    
+):
+    from src.backend.operations.user_operations import operation_register_user
 
     # http method + route + function = endpoint
-    return operation_register_user(user)
+    return operation_register_user(user, session)
 
 
 @app.get("/")
-def read_user_list():
-    return operation_read_user_list()
+def read_user_list(
+    session: Session = Depends(get_session)
+):
+    from src.backend.operations.user_operations import operation_read_user_list
+    
+    return operation_read_user_list(session)
 
 
 @app.post("/login")
-def login_user(user: UserSchema = Body(...)):
-    return operation_login_user(user)
+def login_user(
+    user: UserLoginSchema = Body(...),
+    session: Session = Depends(get_session)
+):
+    from src.backend.operations.user_operations import operation_login_user
+    # {email, password}
+    
+    return operation_login_user(user, session)
 
 
 @app.delete("/{user_id}")
-def delete_user(request: Request, user_id: int):
+def delete_user(
+    request: Request,
+    user_id: int,
+    session: Session = Depends(get_session)
+):
+    from src.backend.tertiary.json_web_tokens import get_decoded_token
+    from src.backend.operations.user_operations import operation_delete_user
+    
     decoded_token_data = get_decoded_token(request)
-    return operation_delete_user(decoded_token_data, user_id)
+    return operation_delete_user(decoded_token_data, user_id, session)
 
 
 # = = = = TASKS = = = =
@@ -77,32 +93,67 @@ class TaskSchema(BaseModel):
 
 
 @app.post("/tasks")
-def create_task(request: Request, task: TaskSchema = Body(...)):
+def create_task(
+    request: Request,
+    task: TaskSchema = Body(...),
+    session: Session = Depends(get_session)
+):
+    from src.backend.operations.task_operations import operation_create_task
+    from src.backend.tertiary.json_web_tokens import get_decoded_token
+    
     decoded_token_data = get_decoded_token(request)
-    return operation_create_task(task, decoded_token_data)
+    return operation_create_task(task, decoded_token_data, session)
 
 
 @app.get("/tasks")
 def read_task_list(
-    request: Request, page: int | None = None, limit: int | None = None
+    request: Request,
+    page: int | None = None,
+    limit: int | None = None,
+    session: Session = Depends(get_session)
 ):
+    from src.backend.operations.task_operations import operation_get_task_list
+    from src.backend.tertiary.json_web_tokens import get_decoded_token
+ 
     decoded_token_data = get_decoded_token(request)
-    return operation_get_task_list(decoded_token_data, page, limit)
+    return operation_get_task_list(decoded_token_data, page, limit, session)
 
 
 @app.get("/tasks/{task_id}")
-def read_task(request: Request, task_id: int):
+def read_task(
+    request: Request, 
+    task_id: int,
+    session: Session = Depends(get_session)
+):
+    from src.backend.operations.task_operations import operation_get_single_task
+    from src.backend.tertiary.json_web_tokens import get_decoded_token
+    
     decoded_token_data = get_decoded_token(request)
-    return operation_get_single_task(task_id, decoded_token_data)
+    return operation_get_single_task(task_id, decoded_token_data, session)
 
 
 @app.put("/tasks/{task_id}")
-def update_task(request: Request, task_id: int, task: TaskSchema = Body(...)):
+def update_task(
+    request: Request,
+    task_id: int,
+    task: TaskSchema = Body(...),
+    session: Session = Depends(get_session)
+):
+    from src.backend.operations.task_operations import operation_update_task
+    from src.backend.tertiary.json_web_tokens import get_decoded_token
+    
     decoded_token_data = get_decoded_token(request)
-    return operation_update_task(task_id, task, decoded_token_data)
+    return operation_update_task(task_id, task, decoded_token_data, session)
 
 
 @app.delete("/tasks/{task_id}")
-def delete_task(request: Request, task_id: int):
+def delete_task(
+    request: Request, 
+    task_id: int,
+    session: Session = Depends(get_session),
+):
+    from src.backend.operations.task_operations import operation_delete_task
+    from src.backend.tertiary.json_web_tokens import get_decoded_token
+    
     decoded_token_data = get_decoded_token(request)
-    return operation_delete_task(task_id, decoded_token_data)
+    return operation_delete_task(task_id, decoded_token_data, session)
