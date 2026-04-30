7b879418aa4c4d459dc8d0d0e60d48ea8bb225bf
dss-neto <192475519+dss-neto@users.noreply.github.com>
2026-04-30 08:25:26 -0300

    Disable relationship data

--- src/backend/database/models.py
+++ src/backend/database/models.py
@@ -1,5 +1,4 @@
 # models is the file where tables are created
-# TODO: figure out how to make this sqlalchemy work
 from sqlalchemy.orm import DeclarativeBase
 
 
@@ -10,7 +9,6 @@ class Base(DeclarativeBase):
 from typing import Optional
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import ForeignKey
-
 # TODO: relationship Task(owner_user_id) and User(id)
 
 
@@ -21,7 +19,9 @@ class User(Base):
     name: Mapped[str]
     email: Mapped[str] = mapped_column(unique=True)
     hashed_password: Mapped[str]
-    tasks: Mapped[list["Task"]] = mapped_column(relationship(back_populates="owner"))
+    # TODO: remove the comment below and try to make it work
+    # tasks: Mapped[list["Task"]] = mapped_column(relationship(back_populates="owner", default=[]))
+    
     # User.tasks will return a list of the tasks attached to this user
     # But it would return in a unreadable format like:
     #   [ <Book at 0x...> , <Book at 0x...>, ... ]
@@ -32,8 +32,9 @@ class User(Base):
     #   User.tasks = list of tasks objects (many objects)
     
     def __repr__(self):
-        task_ids = [task.id for task in self.tasks]
-        return f"User (id={self.id!r}, name={self.name!r}, email={self.email!r}, hashed_password={self.hashed_password!r}, tasks={task_ids!r})"
+        #task_ids = [task.id for task in self.tasks]
+        #return f"User (id={self.id!r}, name={self.name!r}, email={self.email!r}, hashed_password={self.hashed_password!r}, tasks={task_ids!r})"
+        return f"User (id={self.id!r}, name={self.name!r}, email={self.email!r}, hashed_password={self.hashed_password!r})"
 
 # TODO: User.tasks, Task.owner and owner_user_id foreign was added, get an idea of how they work
 
@@ -47,8 +48,11 @@ class Task(Base):
     owner_user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
     # ForeignKey ensures that the owner_user_id is the same as the id of the user tho created
     #   this task
-    owner: Mapped[User] = mapped_column(relationship(back_populates="tasks"))
+    #TODO: remove the comment below and try to make it work
+    # owner: Mapped[User] = mapped_column(relationship(back_populates="tasks"))
+    
     # Task.owner will return the user object attached to the task
 
     def __repr__(self) -> str:
         return f"Task (id={self.id!r}, title={self.title!r}, description={self.description!r}, is_checked={self.is_checked!r}), owner_user_id={self.owner_user_id!r}, owner={self.owner}"
+    
