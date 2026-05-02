22ed879ddd24d151cf6808d2d5f0fc77eaa48259
dss-neto <192475519+dss-neto@users.noreply.github.com>
2026-05-02 07:37:14 -0300

    Add a relationship between User.tasks and Task.owner

--- src/backend/database/models.py
+++ src/backend/database/models.py
@@ -9,7 +9,6 @@ class Base(DeclarativeBase):
 from typing import Optional
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import ForeignKey
-# TODO: relationship Task(owner_user_id) and User(id)
 
 
 class User(Base):
@@ -20,7 +19,10 @@ class User(Base):
     email: Mapped[str] = mapped_column(unique=True)
     hashed_password: Mapped[str]
     # TODO: remove the comment below and try to make it work
-    # tasks: Mapped[list["Task"]] = mapped_column(relationship(back_populates="owner", default=[]))
+    tasks: Mapped[list["Task"]] = relationship(
+            back_populates="owner",
+    )
+    # tasks: Mapped[list["Task"]] = mapped_column(relationship(default=[]))
     
     # User.tasks will return a list of the tasks attached to this user
     # But it would return in a unreadable format like:
@@ -36,23 +38,24 @@ class User(Base):
         #return f"User (id={self.id!r}, name={self.name!r}, email={self.email!r}, hashed_password={self.hashed_password!r}, tasks={task_ids!r})"
         return f"User (id={self.id!r}, name={self.name!r}, email={self.email!r}, hashed_password={self.hashed_password!r})"
 
-# TODO: User.tasks, Task.owner and owner_user_id foreign was added, get an idea of how they work
 
 class Task(Base):
     __tablename__ = "tasks"
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    is_checked: Mapped[int] = mapped_column(default=0)
     title: Mapped[str]
     description: Mapped[Optional[str]]
-    is_checked: Mapped[int] = mapped_column(default=0)
     owner_user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
     # ForeignKey ensures that the owner_user_id is the same as the id of the user tho created
-    #   this task
-    #TODO: remove the comment below and try to make it work
-    # owner: Mapped[User] = mapped_column(relationship(back_populates="tasks"))
+    #   this task (links two tables)
+    
+    owner: Mapped["User"] = relationship(
+            back_populates="tasks"
+    )
     
     # Task.owner will return the user object attached to the task
-
+    # But it's computed by ORM at runtime, not a column in the database
+    
     def __repr__(self) -> str:
         return f"Task (id={self.id!r}, title={self.title!r}, description={self.description!r}, is_checked={self.is_checked!r}), owner_user_id={self.owner_user_id!r}, owner={self.owner}"
-    
