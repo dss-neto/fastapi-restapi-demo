01d346cf82ac3b5325efd843dedc2a6bb782797a
dss-neto <192475519+dss-neto@users.noreply.github.com>
2026-05-02 07:35:56 -0300

    Add commented drop table for easy access

--- src/backend/database/db.py
+++ src/backend/database/db.py
@@ -1,5 +1,9 @@
 from sqlalchemy import create_engine
 from src.backend.database.models import Base
+# from src.backend.database.models import Task
 
 engine = create_engine("sqlite:///src/backend/database/main_database.db")
 Base.metadata.create_all(bind=engine)
+
+
+# Task.__table__.drop(engine)
