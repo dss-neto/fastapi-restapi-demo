ded604ad482102f3dc44484bd583ff6dfde0b3c3
dss-neto <192475519+dss-neto@users.noreply.github.com>
2026-05-07 09:26:51 -0300

    Add role logic

--- src/backend/operations/user_operations.py
+++ src/backend/operations/user_operations.py
@@ -27,19 +27,33 @@ def operation_register_user(
 
     # turns the (password_bytes + salt) hashes back into a string
     hashed_string = hashed.decode("utf-8")
-    
+    # Temporary
+    if user.role == "Admin":
+        role = "Admin"
+    elif user.role == "Owner":
+        role = "Owner"
+    else: role = "Basic"
+        
     user_registered = User(
         name=user.name,
+        role=role,
         email=user.email,
         hashed_password=hashed_string,
     )
+    
     session.add(user_registered)
     session.commit()
-    token = generate_user_token(user_registered.id, user.email)
+    session.refresh(user_registered)
+    token = generate_user_token(user_registered.id, user.email, user.role)
 
     return {
         "message": "You've sucessfuly registered.",
-        "User": user,
+        "User": {
+            "userId": user_registered.id,
+            "name": user_registered.name,
+            "email": user_registered.email,
+            "role": user_registered.role
+        },
         "Token": token,
     }
 
@@ -61,7 +75,7 @@ def operation_login_user(
 
     # compares the encoded passwords
     if bcrypt.checkpw(password_bytes, stored_hash_bytes):
-        token = generate_user_token(user_data.id, user.email)
+        token = generate_user_token(user_data.id, user.email, user_data.role)
         return {
             "message": f"Welcome {user_data.name}! You've successfully logged in.",
             "Token": token,
@@ -85,7 +99,24 @@ def operation_delete_user(
     user_id: int,
     session: Session
 ):
-    if user_id == int(decoded_token_data["sub"]):
+    if decoded_token_data["role"] == "Owner":
+        stmt = delete(Task).where(Task.owner_user_id == user_id)
+        session.execute(stmt)
+        stmt = delete(User).where(User.id == user_id)
+        session.execute(stmt)
+        session.commit()
+        raise_no_content()
+    elif decoded_token_data["role"] == "Admin":
+        stmt = select(User.role).where(User.id == user_id)
+        target_role = session.execute(stmt).scalars().first()
+        if target_role == "Basic" or not target_role:
+            stmt = delete(Task).where(Task.owner_user_id == user_id)
+            session.execute(stmt)
+            stmt = delete(User).where(User.id == user_id)
+            session.execute(stmt)
+            session.commit()
+            raise_no_content()
+    elif user_id == int(decoded_token_data["sub"]):
         stmt = delete(Task).where(Task.owner_user_id == user_id)
         session.execute(stmt)
         stmt = delete(User).where(User.id == user_id)
